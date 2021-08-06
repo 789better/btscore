2927640b0eab8596ff8dfea16ddeb9c79e9f8557
789better <789better@gmail.com>
2021-08-06 15:53:41 +0700

    2 files modified

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 
 
 // Write Javascript code!
+
+$( document ).ready(function() {
+
+  setTimeout(function(){
+    $.ajax({
+      type: 'GET',
+      url: "https://spreadsheets.google.com/feeds/cells/1BJ9WzOLi8J8DNXZo6Zb2ue1Wo2rUgarCRI3mQaZ5KfQ/1/public/full?alt=json",
+      success: function (response) {
+        var entries = response.feed.entry;
+        for (var i = 2; i < entries.length; i = i + 2) {
+          var user = entries[i].content.$t;
+          var score = entries[i+1].content.$t;
+          $('#customers').append('<tr><td>'+user+'</td><td>'+score+'</td></tr>');
+        }
+      }
+    });
+    }, 100);
+
+    $('#customers').append('<tr><td>my data</td><td>more data</td></tr>');
+
+
+  });
