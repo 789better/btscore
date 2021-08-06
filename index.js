be35cf4674803aea95a5a878bf40bf16204b8a69
789better <789better@gmail.com>
2021-08-06 15:55:30 +0700

    1 file modified

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
       url: "https://spreadsheets.google.com/feeds/cells/1BJ9WzOLi8J8DNXZo6Zb2ue1Wo2rUgarCRI3mQaZ5KfQ/1/public/full?alt=json",
       success: function (response) {
         var entries = response.feed.entry;
-        for (var i = 2; i < entries.length; i = i + 2) {
+        for (var i = 2; i < 202; i = i + 2) {
           var user = entries[i].content.$t;
           var score = entries[i+1].content.$t;
           $('#customers').append('<tr><td>'+user+'</td><td>'+score+'</td></tr>');
