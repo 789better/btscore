41904b59b53336ae8b8f15783b92ef30f827b743
789better <789better@gmail.com>
2021-08-21 20:14:05 +0700

    2 files modified

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // Import stylesheets
-import './styles.css';
+// import './styles.css';
 
 // Write Javascript code!
 
-
+var countDownDate = new Date("Aug 28, 2021 11:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML = days + " วัน " + hours + " ชั่วโมง "
+  + minutes + " นาที " + seconds + " วินาที ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "สิ้นสุดกิจกรรมแล้ว ประกาศผล Line: @789BT";
+  }
+}, 1000);
 
 
 
